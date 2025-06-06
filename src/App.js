3412c20f4e55073d480f083e5c7540949892c57b
Ankur Kashyap <kashyap.ankur0114@gmail.com>
2025-06-06 18:44:15 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,8 @@
+function App() {
+    return (
+      <div style={{ padding: "2rem", fontSize: "1.5rem" }}>
+        Hello from OHMAP PG Citrix Team!
+      </div>
+    );
+  }
+  export default App;
